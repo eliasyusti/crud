a34e6c11c094e1e2d80d0ca000be3ba3a01baa5b
elias <eliasyusti@gmail.com>
2019-11-05 18:10:01 -0400

    configuracion de rutas y middlewares

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
+const path = require ('path');
 const app = express();
 
 //configuraciones
 app.set('port', process.env.PORT || 3000);
 
 //middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
 
 //rutas
+app.use('/api/frutas',require('./routes/routes.frutas'));
 
 //archivos estaticos
 
+app.use(express.static(path.join(__dirname,'public')));
 
 //inicializacion del servidor
 app.listen(app.get('port'), () => {
