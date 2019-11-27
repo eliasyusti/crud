9ea7793fb4e0ebd5e87b64f9c05aa6a34cb7a59b
elias <eliasyusti@gmail.com>
2019-11-26 20:02:50 -0400

    funcion eliminar por id flow

--- src/routes/routes.frutas.js
+++ src/routes/routes.frutas.js
@@ -35,7 +35,7 @@ router.put('/:id',async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
 
-        await Fruta.findOneAndRemove(req.params.id);
+        await Fruta.findByIdAndDelete(req.params.id);
         res.json({status: 'fruta eliminada'});
 });
 
