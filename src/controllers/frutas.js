6d938df57e78c09e19e821aafd93bd6226019edc
eliasyusti <eliasyusti@gmail.com>
2021-01-13 19:15:25 -0300

    lasts changes

--- src/controllers/frutas.js
+++ src/controllers/frutas.js
@@ -7,36 +7,36 @@ const User = require('../models/user')
 
 
 frutasControl.getFrutas = (req, res) => {
-        Fruta.find({user: req.params.id}, function(err,frutas) {
-            
-            res.json(frutas); 
+    Fruta.find({ user: req.params.id }, function (err, frutas) {
 
-        }) .populate('user'); 
-    };
-    
+        res.json(frutas);
 
-frutasControl.getFruta = async (req, res) =>{
+    }).populate('user');
+};
+
+
+frutasControl.getFruta = async (req, res) => {
     const fruta = await Fruta.findById(req.params.id);
     res.json(fruta);
 }
 
-frutasControl.postFruta = async (req, res) =>{
-        const { nombre, descripcion} = req.body;
-        const fruta = new Fruta({nombre, descripcion, user: req.params.id});
-        await fruta.save();
-        res.json({status: 'tarea guardada'});
+frutasControl.postFruta = async (req, res) => {
+    const { nombre, descripcion } = req.body;
+    const fruta = new Fruta({ nombre, descripcion, user: req.params.id });
+    await fruta.save();
+    res.json({ status: 'tarea guardada' });
 }
 
-frutasControl.updateFruta = async (req, res) =>{
-        const { nombre, descripcion} = req.body;
-        const newFruta = { nombre, descripcion};
-        await Fruta.findByIdAndUpdate(req.params.id, newFruta);
-        res.json({status: 'informacion actualizada'});
+frutasControl.updateFruta = async (req, res) => {
+    const { nombre, descripcion } = req.body;
+    const newFruta = { nombre, descripcion };
+    await Fruta.findByIdAndUpdate(req.params.id, newFruta);
+    res.json({ status: 'informacion actualizada' });
 }
 
-frutasControl.deleteFruta = async (req, res) =>{
+frutasControl.deleteFruta = async (req, res) => {
     await Fruta.findByIdAndDelete(req.params.id);
-    res.json({status: 'fruta eliminada'});
+    res.json({ status: 'fruta eliminada' });
 }
 
 module.exports = frutasControl;
